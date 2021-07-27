b287f3034dbf361edfb8b988e74f5278db0cf97c
Rodrigo Tripp <rodrigo.tripp@marimekko.com>
2021-07-27 10:23:41 +0300

    removing stuff

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import Description from './Components/Description';
 import Name from './Components/Name';
 import Logos from './Components/Logos';
 
-import { Card, Content, Container } from 'react-bulma-components';
-import styles from './App.module.scss';
-import 'bulma/css/bulma.min.css';
 
 function App() {
   return (
@@ -22,25 +19,6 @@ function App() {
         <Logos/>
         <Description></Description>
       </header>
-      <Container className={ styles.Container} max="true"
-        breakpoint="tablet">
-        <Card className={styles.card_container}>
-          <Card.Image src={me}></Card.Image>
-          <Card.Content>
-            <Content>
-            <div>
-              <p>
-                Mexican, living in Helsinki.
-                Full stack developer with a strong UX and UI development experience.
-              </p>
-              <p>Road cycling, music and bouldering keep me busy.</p>
-              <p>Working for <a href="https://marimekko.com"target="_blank" rel="noopener noreferrer">marimekko</a></p>
-            </div>
-            </Content>
-          </Card.Content>
-        </Card>
-      </Container>
-      
     </div>
   );
 }
--- src/Components/Description.jsx
+++ src/Components/Description.jsx
@@ -6,7 +6,7 @@ function Description() {
       <p>
       Mexican, living in Helsinki.
       Full stack developer with a strong UX and UI development experience.
-      </p><p>Road cycling, music and bouldering keep me busy.</p>
+      </p><p>Bicycles, cycling and music keep me busy.</p>
       <p>Working for <a href="https://marimekko.com"target="_blank" rel="noopener noreferrer">marimekko</a></p>
     </div>
   );

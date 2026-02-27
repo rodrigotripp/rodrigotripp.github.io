7d6905c96f0cdb14d8bbb9b617d7d3482cae57ee
Rodrigo Tripp <rodrigo.trippaguilar@elisa.fi>
2026-02-27 15:39:37 +0200

    adding liquid glass

--- src/Components/NavigationMenu.tsx
+++ src/Components/NavigationMenu.tsx
@@ -1,8 +1,9 @@
 import { SocialIcon } from "react-social-icons";
 import Button from "./Button";
 import { Link } from "react-router";
+import { GlassDiv } from "./GlassDiv";
 
-export const Icons = () => {
+export const Icons: React.FC = () => {
   return (
     <>
       <li>
@@ -29,43 +30,46 @@ type NavigationMenuProps = {
 function NavigationMenu({ onNavigate }: NavigationMenuProps) {
   return (
     <nav className="flex flex-col-reverse gap-5">
-      <ul className="flex flex-wrap justify-start gap-9 md:flex-col text-slate-300 bg-slate-600 rounded-2xl w-80 md:bg-transparent p-1 md:gap-4">
-        <Icons />
+      <ul className="flex flex-wrap justify-start gap-9 md:flex-col rounded-2xl w-80 md:bg-transparent p-1 md:gap-4">
+        <GlassDiv children={<Icons />} hover />
       </ul>
-
       <div className="text-slate-300">
-        <ul className="my-4">
-          <h1>Demo Sites for fun!</h1>
-          <li>
-            <Link
-              to="https://rodrigotripp.vercel.app/meals"
-              className="underline"
-              target="_blank"
-            >
-              Meals with NextJs
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link
-              to="https://meals-vite-vue.vercel.app/"
-              className="text-slate-300 underline"
-              target="_blank"
-            >
-              Meals with Vue/vite
-            </Link>
-          </li>
-        </ul>
-
+        <GlassDiv>
+          <ul className="my-4">
+            <h1>Demo Sites for fun!</h1>
+            <li>
+              <Link
+                to="https://rodrigotripp.vercel.app/meals"
+                className="underline"
+                target="_blank"
+              >
+                Meals with NextJs
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link
+                to="https://meals-vite-vue.vercel.app/"
+                className="underline"
+                target="_blank"
+              >
+                Meals with Vue/vite
+              </Link>
+            </li>
+          </ul>
+        </GlassDiv>
         <div className="text-slate-300">
-          <Button
-            className="underline"
-            children="My work experience"
-            onClick={() => {
-              onNavigate("about");
-              window.scrollTo(0, 0);
-            }}
-          />
+          <br />
+          <GlassDiv hover={true}>
+            <Button
+              className="underline"
+              children="My work experience"
+              onClick={() => {
+                onNavigate("about");
+                window.scrollTo(0, 0);
+              }}
+            />
+          </GlassDiv>
         </div>
       </div>
     </nav>
